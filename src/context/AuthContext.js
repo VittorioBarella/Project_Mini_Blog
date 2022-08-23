42b9a6143f70ebd24cad220c186f1548dea908e6
Vittório <vittorio.br@hotmail.com>
2022-08-23 19:10:47 -0300

    authentication context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children, value }) {
+    <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export function useAuthValue() {
+    return useContext(AuthContext);
+}

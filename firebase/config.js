de89231071c5ed76b8eb42d6f083120cd8726217
Vittório <vittorio.br@hotmail.com>
2022-08-23 14:00:52 -0300

    deleted some useless files and integrated firebase with react

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDoeZPkt6ZwkbXavg6n8Ei2JhYO-1mJB6g",
+    authDomain: "miniblog-e9a24.firebaseapp.com",
+    projectId: "miniblog-e9a24",
+    storageBucket: "miniblog-e9a24.appspot.com",
+    messagingSenderId: "134575735995",
+    appId: "1:134575735995:web:e27810eed4528b9f54159e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };

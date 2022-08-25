803d233ff7643bec404020140f8c65fd5af1c5b6
Vittório <vittorio.br@hotmail.com>
2022-08-24 23:47:08 -0300

    structuring login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,69 @@
+import { useEffect, useState } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 import styles from "./Login.module.css";
-
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const { createUser, error: authError, loading } = useAuthentication();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        const user = {
+            email,
+            password
+        };
+
+        const res = await createUser(user);
+
+        console.log(user);
+    };
+
+    useEffect(() => {
+        if (authError) {
+            setError(authError);
+        }
+    }, [authError]);
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className={styles.login}>
+            <h1>Log in</h1>
+            <p>Log in to use the system</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Email:</span>
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="User email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                </label>
+                <label>
+                    <span>Password:</span>
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                {!loading && <button className="btn">Log in</button>}
+                {loading && (
+                    <button className="btn" disabled>Wait ...</button>
+                )}
+                {error && <p className="error">{error}</p>}
+            </form>
         </div>
     );
 };

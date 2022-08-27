b179e865b070444e7a4ae6ef497e824dabc5fc49
Vittório <vittorio.br@hotmail.com>
2022-08-26 21:08:47 -0300

    entering data in firebase and validating the data

--- src/hooks/useInsertDocument.js
+++ src/hooks/useInsertDocument.js
@@ -1,71 +1,54 @@
-import { collection, Timestamp } from "firebase/firestore";
-import { useEffect } from "react";
-import { useReducer, useState } from "reducer";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { useEffect, useReducer, useState } from "react";
 import { db } from "../firebase/config";
 
 const initialState = {
     loading: null,
-    error: null
+    error: null,
 };
 
 const insertReducer = (state, action) => {
     switch (action.type) {
         case "LOADING":
-            return {
-                loading: true,
-                error: null
-            };
+            return { loading: true, error: null };
         case "INSERTED_DOC":
-            return {
-                loading: false,
-                error: null
-            };
+            return { loading: false, error: null };
         case "ERROR":
-            return {
-                loading: false,
-                error: action.payload
-            };
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
 };
 
-export const useInsertDocument = (docColletion) => {
-
+export const useInsertDocument = (docCollection) => {
     const [response, dispatch] = useReducer(insertReducer, initialState);
 
     // deal with memory leak
     const [cancelled, setCancelled] = useState(false);
 
-    const checkCacelBeforeDispatch = (action) => {
+    const checkCancelBeforeDispatch = (action) => {
         if (!cancelled) {
             dispatch(action);
         }
     };
 
     const insertDocument = async (document) => {
-        checkCacelBeforeDispatch({
-            type: "LOADING",
-        });
-        try {
+        checkCancelBeforeDispatch({ type: "LOADING" });
 
-            const newDocument = { ...document, createAt: Timestamp.now() };
+        try {
+            const newDocument = { ...document, createdAt: Timestamp.now() };
 
             const insertedDocument = await addDoc(
-                collection(db, docColletion),
+                collection(db, docCollection),
                 newDocument
             );
 
-            checkCacelBeforeDispatch({
+            checkCancelBeforeDispatch({
                 type: "INSERTED_DOC",
-                payload: insertedDocument
+                payload: insertedDocument,
             });
-
         } catch (error) {
-            checkCacelBeforeDispatch({
-                type: "ERROR",
-                payload: error.message,
-            });
+            checkCancelBeforeDispatch({ type: "ERROR", payload: error.message });
         }
     };
 
--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,5 +1,9 @@
-import { useState } from "react";
 import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
     const [title, setTitle] = useState("");
@@ -8,8 +12,55 @@ const CreatePost = () => {
     const [tags, setTags] = useState([]);
     const [formError, setFormError] = useState("");
 
+    const { user } = useAuthValue();
+
+    const navigate = useNavigate();
+
+    const { insertDocument, response } = useInsertDocument("posts");
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormError("");
+
+        // validate image
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError("The image must be a URL.");
+        }
+
+        // create tags array
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+        // check values
+        if (!title || !image || !tags || !body) {
+            setFormError("Please fill in all fields!");
+        }
+
+        console.log(tagsArray);
+
+        console.log({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        if (formError) return;
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        // redirect to home page
+        navigate("/");
     };
 
     return (
@@ -21,20 +72,20 @@ const CreatePost = () => {
                     <span>Title:</span>
                     <input
                         type="text"
-                        name="title"
+                        name="text"
                         required
-                        placeholder='Think about a good title...'
+                        placeholder="Think of a good title..."
                         onChange={(e) => setTitle(e.target.value)}
                         value={title}
                     />
                 </label>
                 <label>
-                    <span>Image url:</span>
+                    <span>Image URL:</span>
                     <input
                         type="text"
                         name="image"
                         required
-                        placeholder='Insert an image that represents your post'
+                        placeholder="Insert an image that represents your post."
                         onChange={(e) => setImage(e.target.value)}
                         value={image}
                     />
@@ -47,8 +98,7 @@ const CreatePost = () => {
                         placeholder="Insert post content"
                         onChange={(e) => setBody(e.target.value)}
                         value={body}
-                    >
-                    </textarea>
+                    ></textarea>
                 </label>
                 <label>
                     <span>Tags:</span>
@@ -56,19 +106,20 @@ const CreatePost = () => {
                         type="text"
                         name="tags"
                         required
-                        placeholder='Enter tags separated by commas'
+                        placeholder="Enter tags separated by comma."
                         onChange={(e) => setTags(e.target.value)}
                         value={tags}
                     />
                 </label>
-                <button className="btn">Register</button>
-                {/* {!loading && <button className="btn">Register</button>}
-                {loading && (
+                {!response.loading && <button className="btn">Create post!</button>}
+                {response.loading && (
                     <button className="btn" disabled>
-                        Wait...
+                        Wait.. .
                     </button>
                 )}
-                {error && <p className="error">{error}</p>} */}
+                {(response.error || formError) && (
+                    <p className="error">{response.error || formError}</p>
+                )}
             </form>
         </div>
     );

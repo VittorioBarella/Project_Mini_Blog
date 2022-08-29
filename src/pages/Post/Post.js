08883e2f34baa03cdb1a4f3dedd9db15dae1156b
Vittório <vittorio.br@hotmail.com>
2022-08-29 15:01:31 -0300

    merge fix again cause im stupid

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,34 +1,58 @@
 // CSS
-import styles from "./Post.module.css";
+import styles from "./Home.module.css";
 
 // hooks
-import { useParams } from "react-router-dom";
-import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { Link, useNavigate } from "react-router-dom";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
-const Post = () => {
-    const { id } = useParams();
-    const { document: post } = useFetchDocument("posts", id);
+// react
+import { useState } from "react";
+
+// components
+import PostDetail from "../../components/PostDetail/PostDetail";
+
+const Home = () => {
+    const { documents: posts, loading } = useFetchDocuments("posts");
+
+    const navigate = useNavigate();
+
+    const [query, setQuery] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (query) {
+            return navigate(`/search?q=${query}`);
+        }
+    };
+
+    console.log(loading);
 
     return (
-        <div className={styles.post_container}>
-            {post && (
-                <>
-                    <h1>{post.title}</h1>
-                    <img src={post.image} alt={post.title} />
-                    <p>{post.body}</p>
-                    <h3>Este post trata sobre:</h3>
-                    <div className={styles.tags}>
-                        {post.tags.map((tag) => (
-                            <p key={tag}>
-                                <span>#</span>
-                                {tag}
-                            </p>
-                        ))}
+        <div className={styles.home}>
+            <h1>See our latest posts</h1>
+            <form className={styles.search_form} onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Ou busque por tags..."
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className="btn btn-dark">Search</button>
+            </form>
+            <div className="post-list">
+                {loading && <p>Loading...</p>}
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>No posts found</p>
+                        <Link to="/posts/create" className="btn">
+                            Create first post
+                        </Link>
                     </div>
-                </>
-            )}
+                )}
+                {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+            </div>
         </div>
     );
 };
 
-export default Post;
+export default Home;
